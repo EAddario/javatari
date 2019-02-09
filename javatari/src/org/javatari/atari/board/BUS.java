a1ebc916bd7dc07e543433cc408923cebe2930ff
Ed Addario <29247825+EAddario@users.noreply.github.com>
2019-02-09 18:38:28 +0000

    Refactor BUS

--- javatari/src/org/javatari/atari/board/BUS.java
+++ javatari/src/org/javatari/atari/board/BUS.java
@@ -15,105 +15,100 @@ import org.javatari.utils.Randomizer;
 
 public final class BUS implements BUS16Bits, ClockDriven {
 
-	public BUS(M6502 cpu, TIA tia, PIA pia, RAM ram) {
-		this.cpu = cpu;
-		this.ram = ram;
-		this.tia = tia;
-		this.pia = pia;
-		cpu.connectBus(this);
-		tia.connectBus(this);
-		pia.connectBus(this);
-	}
+    private static final boolean DATA_RETENTION = Parameters.BUS_DATA_RETENTION;
+    public final M6502 cpu;
+    public final RAM ram;
+    public final TIA tia;
+    public final PIA pia;
+    public Cartridge cartridge;
+    private byte data = 0;
+    private boolean cartridgeNeedsClock = false;
+    private boolean cartridgeNeedsBusMonitoring = false;
+    public BUS(M6502 cpu, TIA tia, PIA pia, RAM ram) {
+        this.cpu = cpu;
+        this.ram = ram;
+        this.tia = tia;
+        this.pia = pia;
+        cpu.connectBus(this);
+        tia.connectBus(this);
+        pia.connectBus(this);
+    }
 
-	public void powerOn() {
-		data = 0;
-		if (cartridge == null) {
-			tia.videoOutput().showOSD("NO CARTRIDGE INSERTED!", true);
-			// Data in the bus comes random at powerOn if no Cartridge is present
-			data = (byte)Randomizer.instance.nextInt(256);
-		}
-		// Power on devices connected to the BUS
-		ram.powerOn();
-		pia.powerOn();
-		tia.powerOn();
-		if (cartridge != null) cartridge.powerOn();
-	}
+    public void powerOn() {
+        data = 0;
+        if (cartridge == null) {
+            tia.videoOutput().showOSD("NO CARTRIDGE INSERTED!", true);
+            // Data in the bus comes random at powerOn if no Cartridge is present
+            data = (byte) Randomizer.instance.nextInt(256);
+        }
+        // Power on devices connected to the BUS
+        ram.powerOn();
+        pia.powerOn();
+        tia.powerOn();
+        if (cartridge != null) cartridge.powerOn();
+    }
 
-	public void powerOff() {
-		// Power off devices connected to the BUS
-		if (cartridge != null) cartridge.powerOff();
-		tia.powerOff();
-		pia.powerOff();
-		ram.powerOff();
-	}
+    public void powerOff() {
+        // Power off devices connected to the BUS
+        if (cartridge != null) cartridge.powerOff();
+        tia.powerOff();
+        pia.powerOff();
+        ram.powerOff();
+    }
 
-	@Override
-	public void clockPulse() {
-		pia.clockPulse();
-		cpu.clockPulse();
-		if (cartridgeNeedsClock) cartridge.clockPulse();
-	}
-	
-	@Override
-	public byte readByte(int address) {
-		// CART Bus monitoring
-		if (cartridgeNeedsBusMonitoring) cartridge.monitorBusBeforeRead(address, data);
+    @Override
+    public void clockPulse() {
+        pia.clockPulse();
+        cpu.clockPulse();
+        if (cartridgeNeedsClock) cartridge.clockPulse();
+    }
 
-		if ((address & Cartridge.CHIP_MASK) == Cartridge.CHIP_SELECT) {		// CART selected?
-			if (cartridge != null) data = cartridge.readByte(address);
-		} else if ((address & RAM.CHIP_MASK) == RAM.CHIP_SELECT)			// RAM selected?
-			data = ram.readByte(address);
-		else if ((address & PIA.CHIP_MASK) == PIA.CHIP_SELECT)				// PIA selected?
-			data = pia.readByte(address);
-		else																// TIA selected...
-			// Only bit 7 and 6 are connected to TIA read registers.
-			if (DATA_RETENTION)
-				data = (byte)(data & 0x3f | tia.readByte(address));		// Use the retained data for bits 5-0
-			else
-				data = tia.readByte(address);							// As if all bits were provided by TIA
+    @Override
+    public byte readByte(int address) {
+        // CART Bus monitoring
+        if (cartridgeNeedsBusMonitoring) cartridge.monitorBusBeforeRead(address, data);
 
-		return data;
-	}
+        if ((address & Cartridge.CHIP_MASK) == Cartridge.CHIP_SELECT) {        // CART selected?
+            if (cartridge != null) data = cartridge.readByte(address);
+        } else if ((address & RAM.CHIP_MASK) == RAM.CHIP_SELECT)            // RAM selected?
+            data = ram.readByte(address);
+        else if ((address & PIA.CHIP_MASK) == PIA.CHIP_SELECT)                // PIA selected?
+            data = pia.readByte(address);
+        else                                                                // TIA selected...
+            // Only bit 7 and 6 are connected to TIA read registers.
+            if (DATA_RETENTION)
+                data = (byte) (data & 0x3f | tia.readByte(address));        // Use the retained data for bits 5-0
+            else
+                data = tia.readByte(address);                            // As if all bits were provided by TIA
 
-	@Override
-	public void writeByte(int address, byte b) {
-		// CART Bus monitoring
-		if (cartridgeNeedsBusMonitoring) cartridge.monitorBusBeforeWrite(address, b);
+        return data;
+    }
 
-		data = b;
-		
-		if	((address & TIA.CHIP_MASK) == TIA.CHIP_SELECT) tia.writeByte(address, b);		// TIA selected?
-		else if ((address & RAM.CHIP_MASK) == RAM.CHIP_SELECT) ram.writeByte(address, b);	// RAM selected?
-		else if	((address & PIA.CHIP_MASK) == PIA.CHIP_SELECT) pia.writeByte(address, b);	// PIA selected?
-		else 																				// CART selected...
-			if	(cartridge != null) cartridge.writeByte(address, b);				
-	}
+    @Override
+    public void writeByte(int address, byte b) {
+        // CART Bus monitoring
+        if (cartridgeNeedsBusMonitoring) cartridge.monitorBusBeforeWrite(address, b);
 
-	public void cartridge(Cartridge cartridge) {
-		this.cartridge = cartridge;
-		if (cartridge != null) {
-			data = 0;
-			cartridge.connectBus(this);
-			cartridgeNeedsClock = cartridge.needsClock();
-			cartridgeNeedsBusMonitoring = cartridge.needsBusMonitoring();
-		} else {
-			cartridgeNeedsClock = false;
-			cartridgeNeedsBusMonitoring = false;
-		}
-	}
+        data = b;
 
+        if ((address & TIA.CHIP_MASK) == TIA.CHIP_SELECT) tia.writeByte(address, b);        // TIA selected?
+        else if ((address & RAM.CHIP_MASK) == RAM.CHIP_SELECT) ram.writeByte(address, b);    // RAM selected?
+        else if ((address & PIA.CHIP_MASK) == PIA.CHIP_SELECT) pia.writeByte(address, b);    // PIA selected?
+        else                                                                                // CART selected...
+            if (cartridge != null) cartridge.writeByte(address, b);
+    }
 
-	public Cartridge cartridge;
-	public final M6502 cpu;
-	public final RAM ram;
-	public final TIA tia;
-	public final PIA pia;
-
-	private byte data = 0;
-	private boolean cartridgeNeedsClock = false;
-	private boolean cartridgeNeedsBusMonitoring = false;
-
-
-	private static final boolean DATA_RETENTION = Parameters.BUS_DATA_RETENTION;
+    public void cartridge(Cartridge cartridge) {
+        this.cartridge = cartridge;
+        if (cartridge != null) {
+            data = 0;
+            cartridge.connectBus(this);
+            cartridgeNeedsClock = cartridge.needsClock();
+            cartridgeNeedsBusMonitoring = cartridge.needsBusMonitoring();
+        } else {
+            cartridgeNeedsClock = false;
+            cartridgeNeedsBusMonitoring = false;
+        }
+    }
 
 }
